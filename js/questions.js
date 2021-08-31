d3eb522111528c8cc5922f6c41c762fbf54db0f1
Jeeva Krishna <88268605+Jeevakrishna@users.noreply.github.com>
2021-08-31 19:40:20 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,608 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+     {
+    numb: 1,
+    question: "des cambrioleurs sont entrés dans l'appartement de Vineetha.?",
+    answer: "La semaine dernière",
+    options: [
+      "La semaine dernière",
+      "Le mois dernier",
+      "L'année dernière",
+      "le dernier jour"
+    ]
+  },
+    {
+    numb: 2,
+    question: "............pleure sans cesse.?",
+    answer: "Vineetha",
+    options: [
+      "Shyamala",
+      "Vineetha",
+      "Sangeetha",
+      "Kumar"
+    ]
+  },
+    {
+    numb: 3,
+    question: ".se présente sur la scène de l'accident",
+    answer: "Un gendarme",
+    options: [
+      "Un gendarme",
+      "Un agent d'assurance",
+      "Un vieux homme",
+      "Un médecin"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Selon le témoin, C'est la faute du.........qui a fait l'accident?",
+    answer: "Chauffeur",
+    options: [
+      "Chauffeur",
+      "gendarme ",
+      "piéton ",
+      "mécanicien"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Ce jeune homme a grillé le feu ..............?",
+    answer: "rouge",
+    options: [
+      "Vert",
+      "rouge",
+      "jaune",
+      "gris"
+    ]
+  },
+
+   {
+    numb: 6,
+    question: "mange en lisant les messages sur son portable.?",
+    answer: "Vidya",
+    options: [
+      "Sanjay",
+      "Vidya",
+      "le fils",
+      "a belle – fille"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "On doit être plus prudent en ..............?",
+    answer: "Marchant",
+    options: [
+      "Marchant",
+      "jouant ",
+      "mangeant",
+      "courant"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Ce sont............qui gâtent les enfants.?",
+    answer: "les parents",
+    options: [
+      "les grands-parents",
+      "les parents",
+      "les amis",
+      "les cousins"
+    ]
+  },
+
+ {
+    numb: 9,
+    question: "Elle a beaucoup .............?",
+    answer: "voyagé",
+    options: [
+      "voyager",
+      "voyagé",
+      "voyagée",
+      "voyage"
+    ]
+  },
+
+
+ {
+    numb: 10,
+    question: "Il n'y a plus............ gâteau.?",
+    answer: "de",
+    options: [
+      "du",
+      "de",
+      "de la",
+      "des"
+    ]
+  },
+
+ {
+    numb: 11,
+    question: "couleur voulez-vous essayer ?",
+    answer: "Quelle",
+    options: [
+      "Quel",
+      "Quels ",
+      "Quelle",
+      "Quelles"
+    ]
+  },
+
+ {
+    numb: 12,
+    question: "Saritha achète beaucoup ............romans français?.",
+    answer: "de",
+    options: [
+      "de",
+      "du ",
+      "de la",
+      "des"
+    ]
+  },
+
+
+ {
+    numb: 13,
+    question: ". Les agents d’assurance mènent une .... ?",
+    answer: "enquête",
+    options: [
+      "sondage  ",
+      "observation ",
+      "étude",
+      "enquête"
+    ]
+  },
+
+ {
+    numb: 14,
+    question: "Pourquoi y a-t-il une foule _____ l’appartement de Vineetha ?",
+    answer: "devant",
+    options: [
+      "avant",
+      "auparavant",
+      "devant",
+      "redevant"
+    ]
+  },
+ {
+    numb: 15,
+    question: "Sangeetha remarque ______ devant l’appartement de Vineetha. ",
+    answer: "une foule",
+    options: [
+      "des gens",
+      "du monde",
+      "des personnes",
+      "une foule"
+    ]
+  },
+ {
+    numb: 16,
+    question: "Sangeetha ne sait pas du ______?",
+    answer: "cambriolage",
+    options: [
+      "vol",
+      "cambriolage",
+      "effraction",
+      "pillage"
+    ]
+  },
+
+ {
+    numb: 17,
+    question: "Vineetha est rentrée seulement _____?",
+    answer: "au petit  matin",
+    options: [
+      "à l’aube",
+      "au début du jour ",
+      "à l’aurore",
+      "au petit  matin"
+    ]
+  },
+
+   {
+    numb: 18,
+    question: "Sangeetha trouve ______ horrible.",
+    answer: "le",
+    options: [
+      "le panne de courant",
+      "le",
+      "des cambrioleurs",
+      "le cambriolage"
+    ]
+  },
+
+ {
+    numb: 19,
+    question: "Des cambrioleurs ont ______ tous les objets de valeur ?.",
+    answer: "pillé",
+    options: [
+      "brûlé",
+      "pillé",
+      "fuit",
+      "grillé"
+    ]
+  },
+
+   {
+    numb: 20,
+    question: "Vineetha n’a aucun espoir de ______ ses biens?",
+    answer: "retrouver",
+    options: [
+      "retrouver",
+      "retrouve",
+      "trouver",
+      "trouve"
+    ]
+  },
+
+   {
+    numb: 21,
+    question: "Des cambrioleurs____ par effraction dans l’appartement de Vineetha?",
+    answer: "se sont introduits",
+    options: [
+      "de sont introduits",
+      "se sont introduits",
+      "dans sont introduits",
+      "les sont introduits"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: " Un ______ homme au volant de la voiture a renversé une dame au?",
+    answer: "jeune",
+    options: [
+      "sénile",
+      "jeune",
+      "vieux",
+      "vieux"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "La circulation est bloquée par ______?",
+    answer: "une foule",
+    options: [
+      "des gens",
+      "un accident",
+      "des peuples",
+      "une foule"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: " La pauvre ______ a traversé la rue sans faire attention.?",
+    answer: "dame",
+    options: [
+      "dame",
+      "femme ",
+      "madame ",
+      "fille"
+    ]
+  },
+
+   {
+    numb: 25,
+    question: "_____, ce jeune homme a grillé le feu rouge?",
+    answer: "Monsieur",
+    options: [
+      "Messieurs",
+      "Mesdames",
+      "Madame",
+      "Monsieur"
+    ]
+  },
+
+   {
+    numb: 26,
+    question: "Qu’est-ce ______ s’est passé?",
+    answer: "qui",
+    options: [
+      "que",
+      "qui",
+      "où",
+      "qu'"
+    ]
+  },
+
+   {
+    numb: 27,
+    question: "Mais non, elle a traversé la rue ______ faire attention !",
+    answer: "sans",
+    options: [
+      "aucun",
+      "avec",
+      "sans",
+      "sauf "
+    ]
+  },
+
+
+  {
+    numb: 28,
+    question: "Vous avez ______ comme une folle au milieu de la rue !",
+    answer: "couru",
+    options: [
+      "couru",
+      "courra",
+      "mourra",
+      "pourvu"
+    ]
+  },
+
+
+   {
+    numb: 29,
+    question: "Il a conduit à toute ______ et n’a pas ralenti la voiture.?",
+    answer: "allure",
+    options: [
+      "allure",
+      "allurée",
+      "allurer",
+      "alluré"
+    ]
+  },
+  
+   {
+    numb: 30,
+    question: "Monsieur, je _____ partir maintenant parce que j’ai un rendez-vous important",
+    answer: "dois",
+    options: [
+      "deux",
+      "veux",
+      "peux",
+      "dois"
+    ]
+  },
+
+
+
+  {
+    numb: 31,
+    question: "Un jeune homme au volant de la voiture a ______ une dame au carrefour",
+    answer: "renversé",
+    options: [
+      "renverse",
+      "renverser",
+      "renversée",
+      "renversé"
+    ]
+  },
+
+   {
+    numb: 32,
+    question: " ______ foule bloque la circulation",
+    answer: "une",
+    options: [
+      "un",
+      "du",
+      "une",
+      "le"
+    ]
+  },
+
+   {
+    numb: 33,
+    question: "Monsieur, ______ jeune homme a grillé le feu rouge !",
+    answer: "ce",
+    options: [
+      "ce",
+      "cet",
+      "ces",
+      "cette"
+    ]
+  },
+
+   {
+    numb: 34,
+    question: "Vous avez ______ permis ",
+    answer: "le",
+    options: [
+      "la",
+      "le",
+      "les",
+      "da"
+    ]
+  },
+
+   {
+    numb: 35,
+    question: "Vous n’avez pas ______ appris le code ! ",
+    answer: "bien",
+    options: [
+      "mauvais",
+      "bon",
+      "mal",
+      "bien"
+    ]
+  },
+
+   {
+    numb: 36,
+    question: "______ blague !",
+    answer: "sans",
+    options: [
+      "avec",
+      "sans",
+      "sauf",
+      "aucun"
+    ]
+  },
+ 
+   {
+    numb: 37,
+    question: "On regarde avant ______ traverser la rue ! ",
+    answer: "de",
+    options: [
+      "de",
+      "le",
+      "au",
+      "à"
+    ]
+  },
+
+   {
+    numb: 38,
+    question: " Vous avez couru comme une ______ au milieu de la rue ! ",
+    answer: "folle",
+    options: [
+      "fol",
+      "folle",
+      "fou",
+      "fous"
+     ] 
+  },
+
+   {
+    numb: 39,
+    question: "______ elle est blessée, nous devons appeler une ambulance! ",
+    answer: "Puisqu’",
+    options: [
+      "Contrairement",
+      "l'opposé",
+      "Réciproquement",
+      "Puisqu’"
+    ]
+  },
+
+   {
+    numb: 40,
+    question: "Vous avez couru comme une folle au ______ de la rue!",
+    answer: "milieu",
+    options: [
+      "mille",
+      "milieu",
+      "moyen",
+      "mil"
+    ]
+  },
+   {
+    numb: 41,
+    question: "Non, c’est la ______ du chauffeur ! ",
+    answer: "faute",
+    options: [
+      "faut",
+      "fautes",
+      "faute",
+      "faits"
+    ]
+  },
+   {
+    numb: 42,
+    question: "Monsieur, je dois partir maintenant______j’ai un rendez-vous important",
+    answer: "parce que",
+    options: [
+      "puisque",
+      "parce que",
+      "à cause de",
+      "en raison de"
+    ]
+  },
+   {
+    numb: 43,
+    question: "Mais là, ce n’est pas une excuse ______ renverser quelqu’un !",
+    answer: "pour",
+    options: [
+      "de",
+      "pour",
+      "à",
+      "en"
+    ]
+  },
+  
+   {
+    numb: 44,
+    question: "Vineetha n’a aucun ______ de retrouver ses biens",
+    answer: "espoir",
+    options: [
+      "espoir",
+      "confiance",
+      "espérance",
+      "désespoir"
+    ]
+  },
+   {
+    numb: 45,
+    question: "Des cambrioleurs se sont introduits par effraction dans _____ de Vineetha.",
+    answer: "l’appartement",
+    options: [
+      "le logement",
+      "l’habitation ",
+      "l’appartement",
+      "la maison"
+    ]
+  },
+   {
+    numb: 46,
+    question: "Les agents d’assurance ______ une enquête.",
+    answer: "mènent",
+    options: [
+      "mènent",
+      "dirigent",
+      "conduisent",
+      "guident"
+    ]
+  },
+   {
+    numb: 47,
+    question: "Vineetha n’a aucun espoir de retrouver ses ______. ",
+    answer: "biens",
+    options: [
+      "capitau ",
+      "fortunes",
+      "propriétés",
+      "biens"
+    ]
+  },
+   {
+    numb: 48,
+    question: "Selon ______, ils ont installé un bon système d’alarme dans leurs bâtiments. ",
+    answer:"Sangeetha",
+    options: [
+      "Vineetha",
+      "Sangeetha",
+      "Shyamala",
+      "Vineeth"
+    ]
+  },
+   {
+    numb: 49,
+    question: "Nous avons installé un bon système d’alarme dans ______ bâtiments.",
+    answer: "nos",
+    options: [
+      "nos",
+      "notre",
+      "nous",
+      "nôtre"
+    ]
+  },
+   {
+    numb: 50,
+    question: "______, il y a eu une panne de courant dans l’appartement. ",
+    answer: "Apparemment",
+    options: [
+      "Réellement",
+      "Effectivement",
+      "Apparemment",
+      "Inévitablement"
+    ]
+  },
+ 
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
